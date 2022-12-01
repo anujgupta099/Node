25c573c31006c339acf15a8a533f142ce0a3ecba
anujgupta099 <38115379+anujgupta099@users.noreply.github.com>
2022-12-01 18:16:13 +0530

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const app = express();
 const PORT = process.env.PORT || 3030;
 
 // your code
-console.log(hello world);
+console.log("hello world");
 
 app.listen(PORT, () => {
   console.log(`server started on port ${PORT}`);
